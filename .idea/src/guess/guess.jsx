09955e5e95918ab05189a24e4de7fdb609c1924f
Sc1borg <ericthescienceguy@gmail.com>
2025-10-22 11:25:39 -0600

    reformatted

--- .idea/src/guess/guess.jsx
+++ .idea/src/guess/guess.jsx
@@ -32,7 +32,7 @@ export function Guess() {
       name: guessedCharacter.name === dailyChar.name ? 'green' : 'red',
       school: guessedCharacter.school === dailyChar.school ? 'green' : 'red',
       position: guessedCharacter.position === dailyChar.position ? 'green' : 'red',
-      number: guessedCharacter.number === dailyChar.number ? 'green' : (Math.abs(dailyChar.number - guessedCharacter.number) < 3 ? 'lightblue' : 'yellow')
+      number: guessedCharacter.number === dailyChar.number ? 'green' : (Math.abs(dailyChar.number - guessedCharacter.number) <= 3 ? 'lightblue' : 'yellow')
     };
 
     setGuesses(prevGuesses => [{ character: guessedCharacter, color }, ...prevGuesses]);
@@ -59,33 +59,33 @@ export function Guess() {
         </span>
       </main>
       <div>
-      {/* display the guesses with the highest one being most recent */}
-        {guesses.length > 0 && guesses.map((guess, index) => (
+        {/* display the guesses with the highest one being most recent */}
+        {guesses.length > 0 ? guesses.map((guess, index) => (
           <div key={index} className="guess" style={{ display: 'flex', marginBottom: '10px' }}>
             {/* The little boxes represent the individual parts of the character data.
             I created a "correctness pair" that allows me to remember which pieces were correct even as 
             I move on to new guesses */}
-            <div className="littlebox" style={{ backgroundColor: guess.color.name}}>
+            <div className="littlebox" style={{ backgroundColor: guess.color.name }}>
               <img src={guess.character.photo} alt={guess.character.name} width="50" />
             </div>
             {/* The name and photo use the same check because they should always be the same */}
-            <div className="littlebox" style={{ backgroundColor: guess.color.name}}>
+            <div className="littlebox" style={{ backgroundColor: guess.color.name }}>
               {guess.character.name}
             </div>
             {/* Right now, I only have one schoool, so this will always be green */}
-            <div className="littlebox" style={{ backgroundColor: guess.color.school}}>
+            <div className="littlebox" style={{ backgroundColor: guess.color.school }}>
               {guess.character.school}
             </div>
             {/* This one probably gives you the most information. Everything else is rather unique */}
-            <div className="littlebox" style={{ backgroundColor: guess.color.position}}>
+            <div className="littlebox" style={{ backgroundColor: guess.color.position }}>
               {guess.character.position}
             </div>
             {/* With this one the blue and yellow might be a bit confusing, I'll need to add an explanation thingybob */}
-            <div className="littlebox" style={{ backgroundColor: guess.color.number}}>
+            <div className="littlebox" style={{ backgroundColor: guess.color.number }}>
               {guess.character.number}
             </div>
           </div>
-        ))}
+        )) : null}
       </div>
     </div>
   );
