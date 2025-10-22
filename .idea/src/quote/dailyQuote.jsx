c547b5c0d3dc809de40a70240113df581c0f2cbd
Sc1borg <ericthescienceguy@gmail.com>
2025-10-22 08:26:34 -0600

    quotes works!

---
+++ .idea/src/quote/dailyQuote.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import quotes from "./characterQuotes.json"
+
+export default function getDailyQuote() {
+    const startDate = new Date('2012-02-20');
+    const today = new Date();
+
+    const diffTime = today - startDate;
+    const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+
+    const index = diffDays % quotes.length;
+
+    return (quotes[index]);
+}
--- .idea/src/quote/quote.jsx
+++ .idea/src/quote/quote.jsx
@@ -1,13 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import "./quote.css";
+import getDailyQuote from './dailyQuote';
+import quotes from "./characterQuotes.json"
+import CharSearch from '../guess/charSearch';
 
 export function Quote() {
+  const quote = getDailyQuote()
+  const [guesses, setGuesses] = useState([]);
+  const [gameOver, setGameOver] = useState(false);
+
+  const findCharacterByName = (name) => {
+    return quotes.find(c => c.name === name);
+  };
+
+  const handleGuess = (name) => {
+    if (gameOver) return;
+
+    const guessedCharacter = findCharacterByName(name);
+    if (!guessedCharacter) return;
+
+    if (guessedCharacter.name === quote.name) {
+      setGameOver(true);
+      alert("Congratulations");
+    }
+    if (guesses.length >= 4) {
+      setGameOver(true);
+    }
+    const correctness = {
+      name: guessedCharacter.name === quote.name
+    };
+
+    setGuesses(prevGuesses => [{ character: guessedCharacter, correctness }, ...prevGuesses]);
+  };
+
   return (
-    <div class="box">
+    <div>
+      <div className="bigBox">
         <h1>HAIKYUUDLE</h1>
         <h2>Quote</h2>
         <h2>Eric Jensen</h2>
         <p><a href="https://github.com/Sc1borg/startup">GitHub repo</a></p>
         <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Statistics(placeholder)</a>
+        <p className='quote'>{quote.quote}</p>
+      </div>
+      <div className="categories"><CharSearch onGuess={handleGuess} /></div>
+      <div className="guesses">
+        {guesses.length > 0 && guesses.map((guess, index) => (
+          <div className="littlebox" style={{ backgroundColor: guess.correctness.name ? 'green' : 'red' }}>
+            <img src={guess.character.photo} alt={guess.character.name} width="50" />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
